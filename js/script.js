bb85a598f660a3d2e281dc996d7277bd3fc4664a
pstnv <pstnv@mail.ru>
2024-02-11 17:49:52 +0500

    refactor: make changes

--- js/script.js
+++ js/script.js
@@ -1,98 +1,160 @@
-const input = document.querySelector('#input');
-const btn = document.querySelector('#btn');
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const default_city = "Yekaterinburg";
 
-input.addEventListener('keypress', (e) => {
-    document.querySelector('.notFound').style.visibility = "hidden";
+window.addEventListener("DOMContentLoaded", () => {
+    getForecast(default_city);
+});
+
+input.addEventListener("keypress", (e) => {
+    document.querySelector(".notFound").style.visibility = "hidden";
     if (e.keyCode === 13) {
         enter();
     }
 });
 
-btn.addEventListener('click', enter);
-
-
+btn.addEventListener("click", enter);
 function enter() {
-    if (input.value.trim() !== '') {
-        getData(input.value.trim());
+    if (input.value.trim() !== "") {
+        const city = input.value.trim();
+        getForecast(city);
     }
 }
 
 const api = {
     link: "https://api.openweathermap.org/data/2.5/",
     key: "ddf769883d2c7f595668469959ddb290",
-    geocodingLink: "https://api.openweathermap.org/geo/1.0/"
-}
+    geocodingLink: "https://api.openweathermap.org/geo/1.0",
+};
 
-async function getData (city) {
-    const geocode = await fetch(`${api.geocodingLink}direct?q=${city}&appID=${api.key}`);
-    const geocodeRes = await geocode.json();
-    if (geocodeRes.length !== 0) {
-        const res = await fetch(`${api.link}onecall?lat=${geocodeRes[0].lat}&lon=${geocodeRes[0].lat}&exclude=minutely,hourly,alerts&units=metric&appID=${api.key}`);
-        const result = await res.json();
-        if (result.cod !== '404') {    
-            fillInfo(geocodeRes, result);
+async function getData(link) {
+    try {
+        const response = await fetch(link);
+        if (Math.floor(response.status / 100) !== 2) {
+            throw Error(`Error: ${response.message}`);
         }
-        else {
-            document.querySelector('.notFound').style.visibility = "initial";
-        }
-    }  
-    else {
-        document.querySelector('.notFound').style.visibility = "initial";
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
     }
 }
 
+async function getForecast(city) {
+    const geocodingLink = `${api.geocodingLink}/direct?q=${city}&appid=${api.key}`;
+    const geocodeData = await getData(geocodingLink);
+    if (!geocodeData) {
+        document.querySelector(".notFound").style.visibility = "initial";
+        return;
+    }
+    const [location] = geocodeData;
 
-function fillInfo(geoCode, data) {
-    let weatherPic = document.querySelector('#weatherPic');
-    weatherPic.innerHTML = `<img id="weatherNow" src="https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png">`;
-
-    let city = document.querySelector('#city');
-    city.textContent = `${geoCode[0].name}, ${geoCode[0].country}`;
-
-    getTodayDate(new Date(data.current.dt*1000));
-
-    let temp = document.querySelector('#temp');
-    temp.textContent = `${Math.floor(data.current.temp)}°C`;
-    
-    let feelsLike = document.querySelector('#feelsLike');
-    feelsLike.innerHTML = `Feels like <span>${Math.round(data.current.feels_like)}°C</span>`;
-    
-    let conditions = document.querySelector('#conditions');
-    conditions.innerHTML = `Conditions <span>${data.current.weather[0].description}</span>`;
-    
-    let wind = document.querySelector('#wind');
-    wind.innerHTML = `Wind <span>${Math.round(data.current.wind_speed)} km/h</span>`;
-    
-    let humidity = document.querySelector('#humidity');
-    humidity.innerHTML = `Humidity <span> ${Math.round(data.current.humidity)} %</span>`;
-    
-    let pressure = document.querySelector('#pressure');
-    pressure.innerHTML = `Pressure <span>${Math.round(data.current.pressure)} Pa</span>`;
-
-
-    let daysForecast = document.querySelectorAll('.oneDay');
-    daysForecast.forEach((item, index) => {
-        let soonDay = weekDays[(new Date(data.daily[index+1].dt*1000)).getDay()];
-        item.querySelector('.OneDayHeader').textContent = soonDay;
-        item.querySelector('.soonPic').src = `https://openweathermap.org/img/wn/${data.daily[index+1].weather[0].icon}@2x.png`;
-        item.querySelector('.soonTemp').textContent = `${Math.round(data.daily[index+1].temp.day)}`;
-    });
+    const forecastLink = `${api.link}onecall?lat=${location.lat}&lon=${location.lon}&exclude=minutely,hourly,alerts&units=metric&appID=${api.key}`;
+    const forecastData = await getData(forecastLink);
+    if (!forecastData) {
+        document.querySelector(".notFound").style.visibility = "initial";
+        return;
+    }
+    fillInfo(location, forecastData);
 }
 
+function fillInfo(location, forecast) {
+    const { current: currentForecast, daily: dailyForecast } = forecast;
+    const {
+        dt: today,
+        temp,
+        feels_like,
+        wind_speed,
+        humidity,
+        pressure,
+        weather: [weather],
+    } = currentForecast;
 
-const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-function getTodayDate(today) {
-    const weekDay = document.querySelector('#weekDay');
-    weekDay.textContent = `${weekDays[today.getDay()]}`;
+    const weatherPic = document.querySelector("#weatherPic");
+    weatherPic.innerHTML = `<img id="weatherNow" src="https://openweathermap.org/img/wn/${weather.icon}@2x.png">`;
 
-    const date = document.querySelector('#date');
-    date.textContent = `${today.getDate()}/${today.getMonth()+1}/${today.getFullYear()}`;
-};
+    const city = document.querySelector("#city");
+    city.textContent = `${location.name}, ${location.country}`;
 
-gsap.from(".oneDay", {
-    scrollTrigger: ".oneDay", // start the animation when ".box" enters the viewport (once)
-    y: 100,
-    stagger: 1,
-    duration: .5,
-    opacity: 0
-  });
+    displayTodayData(new Date(today * 1000));
+
+    const tempField = document.querySelector("#temp");
+    tempField.textContent = `${Math.floor(temp)}°C`;
+
+    const feelsLikeField = document.querySelector("#feelsLike");
+    feelsLikeField.innerHTML = `Feels like <span>${Math.round(
+        feels_like
+    )}°C</span>`;
+
+    const conditionsField = document.querySelector("#conditions");
+    conditionsField.innerHTML = `Conditions <span>${weather.description}</span>`;
+
+    const windField = document.querySelector("#wind");
+    windField.innerHTML = `Wind <span>${Math.round(wind_speed)} km/h</span>`;
+
+    const humidityField = document.querySelector("#humidity");
+    humidityField.innerHTML = `Humidity <span> ${Math.round(
+        humidity
+    )} %</span>`;
+
+    const pressureField = document.querySelector("#pressure");
+    pressureField.innerHTML = `Pressure <span>${Math.round(
+        pressure
+    )} Pa</span>`;
+
+    displayDailyForecast(dailyForecast);
+}
+
+const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+function displayTodayData(today) {
+    const day = today.getDay();
+    const date = today.getDate();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+
+    const weekDay = weekDays[day];
+    const weekDayField = document.querySelector("#weekDay");
+    weekDayField.textContent = `${weekDay}`;
+
+    const dateField = document.querySelector("#date");
+    dateField.textContent = `${date}/${month + 1}/${year}`;
+}
+
+function displayDailyForecast(dailyForecast) {
+    const daysForecast = document.querySelector("#coming");
+    const str = dailyForecast.reduce((acc, day, i) => {
+        if (i === 0) {
+            return acc;
+        }
+        const date = new Date(day.dt * 1000).getDay();
+        const weekDay = weekDays[date];
+        const dayIcon = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+        const dayTemp = Math.round(day.temp.day);
+        acc += `        
+                <div class="oneDay">
+                    <h4 class="OneDayHeader">${weekDay}</h4>
+                    <div class="DayData">
+                        <img class="soonPic" src="${dayIcon}">
+                        <p class="soonTemp">${dayTemp}°C</p>
+                    </div>
+                </div>`;
+        return acc;
+    }, "");
+    daysForecast.innerHTML = str;
+}
+
+// gsap.from(".oneDay", {
+//     scrollTrigger: ".oneDay", // start the animation when ".box" enters the viewport (once)
+//     y: 100,
+//     stagger: 1,
+//     duration: 0.5,
+//     opacity: 0,
+// });
