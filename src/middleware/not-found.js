6470319f470dc09bcea2f2a8e6dd51024f2aba7c
pstnv <pstnv@mail.ru>
2024-03-18 01:03:44 +0500

    refactor: update not-found function to simple html code instead file

--- src/middleware/not-found.js
+++ src/middleware/not-found.js
@@ -1,9 +1,8 @@
 const path = require("path");
 
 function notFound(req, res) {
-    res.status(400).sendFile(
-        path.resolve(__dirname, "../public/not-found.html")
-    );
+    res.status(400).type("html").send("<h1>The page you are looking for does not exist</h1>")
+        // sendFile(path.resolve(__dirname, "../public/not-found.html"));
 }
 
 module.exports = notFound;
